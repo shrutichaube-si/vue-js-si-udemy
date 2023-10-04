1fadce819ae995e151341a0af423232dc77cc3b8
shruti chaube <shruti.chaube@Sportzinteractive.net>
2023-10-04 15:11:50 +0530

    added Sports assignment

---
+++ Udemy-vue/vue-advance-components-main/VUE-COMPONENTS/src/components/Person.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1>This is alert component</h1>
+    <p><strong>Full Name</strong>:{{ fullName }}</p>
+</template>
+<script>
+export default{
+    data:()=>({
+        props:['firstname','lastname','age'],
+    }),
+    computed:{
+        fullName(){
+            return this.firstname+" "+this.lastname
+        }
+    }
+
+}
+</script>
+<style scoped>
+
+</style>
