1eecd2e67430f5ffe96fada5cbd899f13c0f6bff
DhruvS-SI <140160521+DhruvS-SI@users.noreply.github.com>
2023-10-05 11:55:52 +0530

    Third Commit - Vanilla Javascript

--- tags-input/src/main.js
+++
@@ -1,9 +0,0 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import TagsInput from "./components/TagsInput.vue"
-
-const app = createApp(App)
-app.component("tags-input",TagsInput);
-
-app.mount('#app')
