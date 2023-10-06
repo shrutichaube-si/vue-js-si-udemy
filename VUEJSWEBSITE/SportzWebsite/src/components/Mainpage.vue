194ced469796ec66e9d8f57714c2a7e4849fe1b8
SushmaD-SI <sushma.devadiga@sportzinteractive.net>
2023-10-06 16:03:36 +0530

    API,Search,Dropdown Functionality

---
+++ VUEJSWEBSITE/SportzWebsite/src/components/Mainpage.vue
@@ -0,0 +1,121 @@
+<template>
+    <div>
+      <div class = "all-class">
+      <h1>Batsman</h1>
+      
+      <div class="col">
+        <div class="card" v-for="(player, index) in filteredPlayer(1)" :key="index">
+          <img :src="player.image" class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">{{ player.name  }}</h5>
+            <p class="card-text">Matches: {{ player.matches }}</p>
+            <p class="card-text">Runs: {{ player.runs }}</p>
+            <p class="card-text">50/100s: {{ player['50s'] }}/{{ player['100s'] }}</p>
+            <p class="card-text">Highest Score: {{ player.highest_score }}</p>
+            <p class="card-text">Team Name: {{ player.team_name }}</p>
+            <p class="card-text">Best Bowling Figures: {{ player.best_bowling_figures }}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class ="all-class">
+    <h1>Bowler</h1>
+    <div class="col">
+      <div class="card" v-for="(player, index) in filteredPlayer(3)" :key="index">
+        <img :src="player.image" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">{{ player.name  }}</h5>
+          <p class="card-text">Matches: {{ player.matches }}</p>
+          <p class="card-text">Runs: {{ player.runs }}</p>
+          <p class="card-text">50/100s: {{ player['50s'] }}/{{ player['100s'] }}</p>
+          <p class="card-text">Highest Score: {{ player.highest_score }}</p>
+          <p class="card-text">Team Name: {{ player.team_name }}</p>
+          <p class="card-text">Best Bowling Figures: {{ player.best_bowling_figures }}</p>
+        </div>
+      </div>
+    </div>
+    </div>
+    <div class ="all-class" >
+    <h1>Allrounder</h1>
+    <div class="col">
+      <div class="card" v-for="(player, index) in filteredPlayer(2)" :key="index">
+        <img :src="player.image" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">{{ player.name  }}</h5>
+          <p class="card-text">Matches: {{ player.matches }}</p>
+          <p class="card-text">Runs: {{ player.runs }}</p>
+          <p class="card-text">50/100s: {{ player['50s'] }}/{{ player['100s'] }}</p>
+          <p class="card-text">Highest Score: {{ player.highest_score }}</p>
+          <p class="card-text">Team Name: {{ player.team_name }}</p>
+          <p class="card-text">Best Bowling Figures: {{ player.best_bowling_figures }}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+    </div>
+  </template>
+  
+  <script>
+ 
+
+  export default {
+    props: ['playersData'],
+  
+    computed: {
+      filteredPlayer() {
+
+        // console.log(this.playersData);
+         return (role) => {
+        
+          return this.playersData.filter((player) => player.role === role);
+        }; 
+      },
+    },
+    
+  };
+ 
+  
+
+  </script>
+  
+  <style>
+  /* Your CSS styles */
+  </style>
+  
+
+<!-- <template>
+    <div>
+      <div v-for="(player, index) in playersData" :key="index">
+        <h1 v-if="player.role === 2">Batsman</h1>
+        <h1 v-else-if="player.role === 4">Bowler</h1>
+        <h1 v-else-if="player.role === 3">Allrounder</h1>
+  
+        <div class="col">
+          <div class="card">
+            <img :src="player.image" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">{{ player.name }}</h5>
+              <p class="card-text">Matches: {{ player.matches }}</p>
+              <p class="card-text">Runs: {{ player.runs }}</p>
+              <p class="card-text">50/100s: {{ player['50s'] }}/{{ player['100s'] }}</p>
+              <p class="card-text">Highest Score: {{ player.highest_score }}</p>
+              <p class="card-text">Team Name: {{ player.team_name }}</p>
+              <p class="card-text">Best Bowling Figures: {{ player.best_bowling_figures }}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: ['playersData'],
+  };
+  </script>
+  
+  <style>
+  /* Your CSS styles */
+  </style>
+   -->
+   
---
+++ VUEJSWEBSITE/SportzWebsite/src/components/Navbar.vue
@@ -0,0 +1,62 @@
+<template>
+    <div>
+    <h1>SPORTZ INTERACTIVE</h1>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Player Details</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <select id="team"  v-model="selectedTeam" @change="selectTeam">
+                      <option selected>ALL</option>
+                      <option value="IND">IND</option>
+                      <option value="AUS">AUS</option>
+                      <option value="PAK">PAK</option>
+                      <option value="ENG">ENG</option>
+                    </select>
+                    <div class="container">
+                      <div id="IND" class="team-row"></div>
+                      <div id="AUS" class="team-row"></div>
+                      <div id="PAK" class="team-row"></div>
+                      <div id="ENG" class="team-row"></div>
+                    </div>
+
+                    <br><br>
+                </ul> 
+                <!--<button onClick="window.location.reload();">UPDATE</button>-->
+                <!-- <div class="form-group">
+                 <label for="key">Search Player:</label>
+                <input class="form-control" type="text" id="key" v-model="searchKeyword" placeholder="Enter player/team name">
+            </div> -->
+            <form class="d-flex" role="search">
+              <input class="form-control me-2" v-model="sText" type="search" placeholder="Search" id = "key" aria-label="Search">
+              
+            </form>
+            <button class="btn btn-outline-success" @click="passText" type="submit">Search</button>
+          </div>
+        </div>
+      </nav>
+    </div>
+</template>
+<script>
+export default {
+  data : () =>({
+    selectedTeam: 'ALL',
+    sText:""
+  }),
+  props: ['playersData'],
+  methods:{
+    passText(){
+      this.$emit("searchText",this.sText)
+    },
+    selectTeam() {
+      this.$emit('updateSelectedTeam', this.selectedTeam);
+    },
+  }
+}
+</script>
+
+
+
