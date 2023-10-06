9253a7add9c850805f201003c476c7e96644f7f7
DhruvS-SI <140160521+DhruvS-SI@users.noreply.github.com>
2023-10-06 11:20:57 +0530

    Dropdown & API

--- players-app/src/App.vue
+++ players-app/src/App.vue
@@ -17,12 +17,12 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <select v-model="selectedTeam">
+            <select @click="dropPlayers" v-model="selectedTeam">
               <option value="ALL">ALL</option>
-              <option value="India">IND</option>
-              <option value="Pakistan">PAK</option>
-              <option value="Australia">AUS</option>
-              <option value="England">ENG</option>
+              <option value="IND">IND</option>
+              <option value="PAK">PAK</option>
+              <option value="AUS">AUS</option>
+              <option value="ENG">ENG</option>
             </select>
           </ul>
           <form class="d-flex">
@@ -63,6 +63,7 @@
   </div> 
 </template>
 <script>
+import axios from 'axios';
 import PlayerList from "./components/PlayerList.vue";
 export default {
   data() {
@@ -70,166 +71,20 @@ export default {
       selectedTeam: "ALL",
       search: "",
       originalPlayers: [
-      {
-        name: 'Virat Kohli',
-        matches: '101',
-        role: 2,
-        runs: '10234',
-        '50s': '23',
-        '100s': '46',
-        highest_score: '183',
-        best_bowling_figures: 0,
-        team_id: 4,
-        team_name: 'IND',
-        image: 'https://resources.pulse.icc-cricket.com/players/130x150/164.png',
-      },
-      {
-        name: 'Hardik Pandya',
-        matches: '100',
-        role: 3,
-        runs: '10234',
-        '50s': '23',
-        '100s': '46',
-        highest_score: '183',
-        best_bowling_figures: '1-23',
-        team_id: 4,
-        team_name: 'IND',
-        image: 'https://resources.pulse.icc-cricket.com/players/130x150/2740.png',
-      },
-      {
-        name: 'Jasprit Bumrah',
-        matches: '100',
-        role: 4,
-        runs: '10234',
-        '50s': '23',
-        '100s': '46',
-        highest_score: '183',
-        best_bowling_figures: '5-23',
-        team_id: 4,
-        team_name: 'IND',
-        image: 'https://resources.pulse.icc-cricket.com/players/130x150/1124.png',
-      },
-      {
-        name: 'David Warner',
-        matches: '100',
-        role: 2,
-        runs: '10234',
-        '50s': '23',
-        '100s': '46',
-        highest_score: '183',
-        best_bowling_figures: 0,
-        team_id: 1,
-        team_name: 'AUS',
-        image: 'https://resources.pulse.icc-cricket.com/players/210/170.png',
-      },
-      {
-        name: 'Glen Maxwell',
-        matches: '100',
-        role: 3,
-        runs: '10234',
-        '50s': '23',
-        '100s': '46',
-        highest_score: '183',
-        best_bowling_figures: '1-23',
-        team_id: 1,
-        team_name: 'AUS',
-        image: 'https://resources.pulse.icc-cricket.com/players/130x150/282.png',
-      },
-      {
-        name: 'Pat Cummings',
-        matches: '100',
-        role: 4,
-        runs: '10234',
-        '50s': '23',
-        '100s': '46',
-        highest_score: '183',
-        best_bowling_figures: '5-23',
-        team_id: 1,
-        team_name: 'AUS',
-        image: 'https://resources.pulse.icc-cricket.com/players/130x150/488.png',
-      },
-      {
-        name: 'Babar Azam',
-        matches: '100',
-        role: 2,
-        runs: '10234',
-        '50s': '23',
-        '100s': '46',
-        highest_score: '183',
-        best_bowling_figures: 0,
-        team_id: 6,
-        team_name: 'PAK',
-        image: 'https://resources.pulse.icc-cricket.com/players/130x150/2759.png',
-      },
-      {
-        name: 'Shaheen Afridi',
-        matches: '100',
-        role: 3,
-        runs: '10234',
-        '50s': '23',
-        '100s': '46',
-        highest_score: '183',
-        best_bowling_figures: '1-23',
-        team_id: 6,
-        team_name: 'PAK',
-        image: 'https://resources.pulse.icc-cricket.com/players/130x150/4530.png',
-      },
-      {
-        name: 'Haris Rauf',
-        matches: '100',
-        role: 4,
-        runs: '10234',
-        '50s': '23',
-        '100s': '46',
-        highest_score: '183',
-        best_bowling_figures: '5-23',
-        team_id: 6,
-        team_name: 'PAK',
-        image: '',
-      },
-      {
-        name: 'Jhonny Bairstow',
-        matches: '100',
-        role: 2,
-        runs: '10234',
-        '50s': '23',
-        '100s': '46',
-        highest_score: '183',
-        best_bowling_figures: 0,
-        team_id: 5,
-        team_name: 'ENG',
-        image: 'https://resources.pulse.icc-cricket.com/players/130x150/506.png',
-      },
-      {
-        name: 'Ben Stokes',
-        matches: '100',
-        role: 3,
-        runs: '10234',
-        '50s': '23',
-        '100s': '46',
-        highest_score: '183',
-        best_bowling_figures: '1-23',
-        team_id: 5,
-        team_name: 'ENG',
-        image: 'https://resources.pulse.icc-cricket.com/players/130x150/1154.png',
-      },
-      {
-        name: 'James Anderson',
-        matches: '100',
-        role: 4,
-        runs: '10234',
-        '50s': '23',
-        '100s': '46',
-        highest_score: '183',
-        best_bowling_figures: '5-23',
-        team_id: 5,
-        team_name: 'ENG',
-        image: 'https://resources.pulse.icc-cricket.com/players/130x150/900.png',
-      },
       ],
       players: [],
     };
   },
+mounted()
+{
+  axios.get('http://127.0.0.1:5500/players.json')
+  .then((resp)=>{
+    console.warn(resp.data.originalPlayers)
+    this.originalPlayers = resp.data.originalPlayers
+    this.dropPlayers()
+  })
+  
+},
   components: {
     PlayerList,
     
@@ -248,6 +103,10 @@ export default {
     },
   },
   methods: {
+    dropPlayers(){
+      this.players = this.originalPlayers.filter(player=>(player.team_name.toLowerCase().includes(this.selectedTeam.toLowerCase()))||(this.selectedTeam=="ALL"))
+    },
+
     searchPlayers() {
       this.players = this.originalPlayers.filter((player) => {
         const playerName = player.name.toLowerCase();
