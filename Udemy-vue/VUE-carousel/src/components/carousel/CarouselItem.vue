4d496f4eb57cfaf84794d2f54268f2b0b3189162
shruti chaube <shruti.chaube@Sportzinteractive.net>
2023-10-10 14:07:13 +0530

    Completed Carousel

--- Udemy-vue/VUE-carousel/src/components/carousel/CarouselItem.vue
+++ Udemy-vue/VUE-carousel/src/components/carousel/CarouselItem.vue
@@ -1,19 +1,52 @@
 <template>
-     <div class="carousel-item" v-show="currentSlide===index">
+   <transition :name="transitionName">
+     <div class="carousel-item" v-show="currentSlide===index" 
+     @mouseenter="$emit('mouseenter')" 
+     @mouseout="$emit('mouseout')"
+     >
         <img :src="slide">
      </div>
+   </transition>
 </template>
 <script>
 export default{
-   props:['slide','currentSlide',"index"],
+   emits:['mouseenter','mouseout'],
+   props:['slide','currentSlide',"index","direction"],
+   computed:{
+      transitionName(){
+         return this.direction ==="right"?"slide-out":"slide-in";
+      }
+   }
    }
 </script>
 <style scoped>
 .carousel-item{
-   position: absolute;
+   position:absolute;
    top:0;
-   left: 0;
+   left:0;
    right:0;
    bottom:0;
+   padding-top: 50px;
+   padding-left: 200px;
+   
 }
+.slide-out-enter-active,
+.slide-out-leave-active,
+.slide-in-enter-active,
+.slide-in-leave-active {
+  transition: all 1s ease-in-out;
+}
+.slide-out-enter-from {
+  transform: translateX(100%);
+}
+.slide-out-leave-to {
+  transform: translateX(-100%);
+}
+.slide-in-enter-from {
+  transform: translateX(-100%);
+}
+.slide-in-leave-to {
+  transform: translateX(100%);
+}
+
 </style>
