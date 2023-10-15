596f47de605eb2d0114472edd0b9e56a07a59310
shruti chaube <shruti.chaube@Sportzinteractive.net>
2023-10-16 00:15:27 +0530

    Added Automcomplete and Googl drive part 1

--- Udemy-vue/AUTOCOMPLETE-COMP/src/App.vue
+++ Udemy-vue/AUTOCOMPLETE-COMP/src/App.vue
@@ -1,12 +1,18 @@
 <template>
-<div>
- 
-<auto-complete></auto-complete>
-</div>
+ <div class="w-screen h-screen bg-gray-200 flex flex-col justify-center items-center">
+    <div class="w-1/3">
+      <h1 class="text-center mb-3 font-bold">Selected {{ country }}</h1>
+      
+      <auto-complete :source="countries" v-model="country"></auto-complete>
+    </div>
+  </div>
 </template>
 <script setup>
+import countries from './data/countries.json'
 import AutoComplete from './components/autocomplete/AutoComplete.vue';
+import { ref } from 'vue';
 
+const country=ref('')
 </script>
 <style scoped>
 
--- Udemy-vue/AUTOCOMPLETE-COMP/src/components/autocomplete/AutoComplete.vue
+++ Udemy-vue/AUTOCOMPLETE-COMP/src/components/autocomplete/AutoComplete.vue
@@ -1,10 +1,57 @@
 <template>
-    <div>
-        <input type="text">
+    <div class="w-full relative">
+       <input
+        type="text" 
+        :value="modelValue" 
+        @input="handleInput"
+        class="px-5 py-3 w-full border border-gray-300 rounded-md">
+       <ul v-show="searchResults.length && isOpen"
+       class ="mt-1 w-full max-h-60 border border-gray-300 rounded-md bg-white absolute overflow-y-auto"
+       >
+        <li v-for="result in searchResults" 
+         :key="result.name" 
+        @click="setSelectd(result.name)"
+        class="px-4 py-3 border-b border-gray-200 text-stone-600 cursor-pointer hover:bg-gray-100 transition-colors"
+        >{{ result.name }}</li>
+       </ul>
     </div>
 </template>
 
-<script>
+<script setup >
+import { computed, ref } from 'vue';
+
+const props =defineProps({
+    source:{
+        type:Array,
+        required:true,
+        default:()=>[]
+    },
+    modelValue:String
+})
+  const emit =defineEmits(['update:modeValue'])
+  const search= ref('')
+ const isOpen = ref(false)
+const searchResults = computed(()=>{
+    if(search.value===''){
+        return []
+    }
+    return props.source.filter(item=>{
+        if(item.name.toLowerCase().includes(search.value.toLowerCase())){
+            return item
+        }
+    })
+})
+
+const setSelectd = item=>{
+    search.value = item
+    isOpen.value=false
+    emit('update:modelValue', search.value)
+}
+const handleInput = event=>{
+    search.value = event.target.value
+    isOpen.value=true
+    emit('update:modelValue', search.value)
+}
 
 </script>
 
