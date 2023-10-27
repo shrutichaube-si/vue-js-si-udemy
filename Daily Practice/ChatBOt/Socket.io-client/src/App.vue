fa8d3dfeffefa09b2e6b1f9cbe0f312a5472a64f
darpanchaudhari-si <darpan.chaudhari@sportzinteractive.net>
2023-10-27 12:39:56 +0530

    ChatBot completed

---
+++ Daily Practice/ChatBOt/Socket.io-client/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <ChatApp></ChatApp>
+</template>
+
+<script>
+import ChatApp from './components/ChatApp.vue';
+export default {
+    name: "App",
+    components: {
+         ChatApp
+     },
+};
+
+</script>
+
+<style>
+body {
+    margin: 0px;
+}
+
+#app {
+    font-family: Arial, Helvetica, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    color: #2c3e50;
+    margin-top: 10px;
+}
+
+</style>
