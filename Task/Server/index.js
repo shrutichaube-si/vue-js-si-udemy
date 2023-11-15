19801376f091f3cd2219caac937e9429a28d686c
Dhruv Solanki <dhruv.solanki@sportzinteractive.net>
2023-11-15 23:22:33 +0530

    Updated Changes

--- Task/Server/index.js
+++ Task/Server/index.js
@@ -1,19 +1,20 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path')
-
-
 const multer = require('multer')
-
 const PORT = 8080;
 const app = express();
-
 app.use(express.json())
-
 const storage = multer.diskStorage({
-    dest: (req,file,cb) => {
-        cb(null,'./Images')
-
+    destination: (req,file,cb) => {
+        const dest = './Images';
+        fs.mkdir(dest,{recursive:true},(err) => {
+            if(err){
+                cb(err,null);
+            }else{
+                cb(null,dest);
+            }
+        })
     },
      filename:(req,file,cb) => {
         console.log(file)
@@ -34,15 +35,11 @@ const upload = multer({
         }
     }
 })
-
 app.listen(PORT,() => 
     console.log(`Running this application on the PORT ${PORT}`)
 );
-
 app.use(express.json());
-// Reading the data
 const data = require('./db.json');
-
 app.get('/getRequest',(req,res) => {
     res.send("Hello there!")
 })
@@ -50,33 +47,24 @@ app.get('/continents', (req, res) => {
     const continents = data.continents;
     res.json(continents);
   });  
-
-
 app.get('/countries', (req, res) => {
   const countries = data.continents.map((continent) => continent.countries.map((country) => (country.name)))  
-//const countriess = data.continents.map((continent) => {continent.countries.filter((country) =>(country.name))})
 const countriess = data.continents.map((continent) => continent.countries.map((country) => country.name)).flat();
   console.log(countriess)
   res.json(countriess);
 });
-
 app.get('/countries/:id',(req,res) => {
       const { id } = req.params
       let ele;
-    // const cid = data.continents.filter((continent) => continent.countries.find((country) => country.id === parseInt(id)))
     const cid = data.continents.map((continent) => {
          continent.countries.filter((country) => {
             if(country.id == id ){ ele = country }
-            // console.log(ele)
             return country.id == id
         })
     }
         )
     res.json(ele);
-    // console.log(id);
-    //console.log(ele);
 })
-
 app.get('/countriess', (req, res) => {
     const cd = data.continents.flatMap((continent) => 
         continent.countries.map((country) => ({
@@ -88,27 +76,30 @@ app.get('/countriess', (req, res) => {
     );
     res.json(cd);
 });
-
-app.post('/countries', upload.single("images") ,(req,res) => {
+app.post('/countries', upload.single("image") ,(req,res) => {
     console.log("hii" + req.body.countryName)
 const {
         name,
         id,
-        rank,
-        image
+        rank
     } = req.body;
-    
+    const image = req.file;
     if(!name || !id || !rank || !image){
-        res.status(400).json({message: 'Name,id,rank, & image are required'})
         console.log({message: 'Name,id,rank, & image are required'})
+       return res.status(400).json({message: 'Name,id,rank, & image are required'})
+        
     }else{
-        data.continents[0].countries.push({name,id,rank,image});
-    }
+        const selectedContinentIndex = data.continents.findIndex(continent => continent.name === req.body.selectedContinent);
+        if(selectedContinentIndex !== -1){
+            data.continents[selectedContinentIndex].countries.push({name,id,rank,image});
    fs.writeFileSync('db.json',JSON.stringify(data,null,2));
-   res.json({message:'Country added successfully',country:{name,id,rank,image}})
    console.log({name,id,rank,image})
+   return res.json({message:'Country added successfully',country:{name,id,rank,image}})
+   
+}else{
+    return res.status(404).json({message:'Selected Continent not found'})
+}
     }
+}
 )
- //writing the updated data back to the json file using fs and writesyncfile
- //The JSON.stringify method converts a JavaScript object or value to a JSON string.
- //res.status(400).json({message: 'Name,id,rank, & image are required'}) //for much cleaner code.
+
--- Task/src/components/Countries.vue
+++ Task/src/components/Countries.vue
@@ -3,20 +3,18 @@
     <div class="row">
       <div class="col-md-6">
         <div class="form-group">
-          <label for="continents">Choose a Continent: {{ selectedContinent }}</label>
+          <label for="continents">Choose a Continent:</label>
           <select id="continents" class="form-control" v-model="selectedContinent">
             <option v-for="continent in continents" :key="continent.name">{{ continent }}</option>
           </select>
         </div>
-
         <div class="form-group">
-          <label for="countries">Choose a Country: {{ selectedCountry }}</label>
+          <label for="countries">Choose a Country: </label>
           <select id="countries" class="form-control" v-model="selectedCountry">
             <option v-for="country in filteredCountries" :key="country">{{ country }}</option>
           </select>
         </div>
       </div>
-
       <div class="col-md-6">
         <div v-if="getCountryData" class="card" style="width: 18rem;">
             <h3 class="card-title">Country Details</h3>
@@ -26,51 +24,38 @@
             <p class="card-text">Rank: {{ getCountryData.rank }}</p>
           </div>
         </div>
-
-        <!-- <div v-if="getCountryData" class="mt-4">
-          <h2>The list of All Selected Countries</h2>
-          <h4>Country Names: {{ getCountryData.name }}</h4>
-          <p>ID: {{ getCountryData.id }}</p>
-          <img :src="getCountryData.image" alt="Country Image" class="img-fluid"/>
-          <p>Rank: {{ getCountryData.rank }}</p>
-        </div> -->
       </div>
     </div>
-
     <hr class="my-4">
-
-    <div class="row">
-      <div class="col-md-6">
-        <div class="form-group">
-          <label>Enter the id:</label>
-          <input type="text" class="form-control" v-model="id"/>
-        </div>
-
-        <div class="form-group">
-          <label>Enter the Name of the country:</label>
-          <input type="text" class="form-control" v-model="countryName"/>
-        </div>
+    <form enctype="multipart/form-data" method="post">
+  <div class="row">
+    <div class="col-md-6">
+      <div class="form-group">
+        <label>Enter the id:</label>
+        <input type="text" class="form-control" v-model="id"/>
       </div>
-
-      <div class="col-md-6">
-        <div class="form-group">
-          <label>Input the image:</label>
-          <input type="file" class="form-control-file" @change="onFileChange"/>
-        </div>
-
-        <div class="form-group">
-          <label>Enter the Rank:</label>
-          <input type="text" class="form-control" v-model="rank"/>
-        </div>
-
-        <button type="submit" class="btn btn-primary" @click="addCountry">Add Country</button>
+      <div class="form-group">
+        <label>Enter the Name of the country:</label>
+        <input type="text" class="form-control" v-model="countryName"/>
       </div>
     </div>
+    <div class="col-md-6">
+      <div class="form-group">
+        <label>Input the image:</label>
+        <input type="file" class="form-control-file" @change="onFileChange"/>
+      </div>
+      <div class="form-group">
+        <label>Enter the Rank:</label>
+        <input type="text" class="form-control" v-model="rank"/>
+      </div>
+      <button type="submit" class="btn btn-primary" @click.prevent="addCountry">Add Country</button>
+    </div>
+  </div>
+</form>
   </div>
   </template>
 <script>
 import axios from 'axios';
-
 export default {
     name: "App",
     data() {
@@ -93,24 +78,18 @@ export default {
     filteredCountries() {
         console.log(this.countries,'this.countries')
         console.log(this.selectedContinent,'this.selectedContinent')
-        // console.log(country,'country')
-
-        // clg
         if (!this.selectedContinent) {
             return this.countries;
         } else {
             console.log(this.continentsWithCountry,'this.continentsWithCountry')
             const selectedContinentObject=Object.keys(this.continentsWithCountry).find((continent)=>{
                 console.log('continent-------------',this.continentsWithCountry[continent])
-                // console.log('continent-------------',this.continentsWithCountry[continent])
-
                 return this.continentsWithCountry[continent].name==this.selectedContinent
             })
-            console.log(selectedContinentObject,'+++++')
+            console.log(selectedContinentObject,'selectedContinentObject')
             return this.continentsWithCountry[selectedContinentObject].countries.map((country)=>{
                 return country.name
             })
-            //  return this.continentsWithCountry.filter((country) => country.name === this.selectedContinent);
         }
     },
     getCountryData(){
@@ -124,21 +103,13 @@ export default {
     try {
         const Continentresponse = await axios.get('http://localhost:8080/continents'); 
         const Countryresponse = await axios.get('http://localhost:8080/countries');
-        
         const CountryData = await axios.get('http://localhost:8080/countriess')
         this.countryData = CountryData.data;
-
         this.continentsWithCountry=Continentresponse.data;
-    
         this.continents = Continentresponse.data.map((continent) => continent.name); 
-        
         this.countries = Countryresponse.data;
-
-      
         console.log(this.continents);
         console.log(this.countries);
-   
-        
     } catch (error) {
         console.error('Error Fetching Data:', error);
     }
@@ -148,29 +119,36 @@ methods:{
         this.file = e.target.files[0];
     },
     addCountry(){
-        console.log(this.file,'drftgyhuji')
-        const newCountryData = {
-            id: this.id,
-            name: this.countryName,
-            image: this.file,
-            rank: this.rank
-        };
-        this.PostCountryData(newCountryData);
+        console.log(this.file,'Check if its working!!')
+        if(this.file){
+          const fileType = /jpeg|jpg|png/;
+          const mimeType = fileType.test(this.file.type);
+          const extname = fileType.test(this.file.name.toLowerCase());
+        if(mimeType && extname){
+          const formData = new FormData();
+        formData.append('id',this.id);
+        formData.append('name',this.countryName);
+        formData.append('image',this.file);
+        formData.append('rank',this.rank);
+        formData.append('selectedContinent',this.selectedContinent);  
+        this.PostCountryData(formData);
+        }else{
+          alert(`Error: Image with ${fileType} Type & size upto 4 MB only allowed!`)
+        }
+        }else{
+          alert("Error: Please Select an image file")
+        }
     },
-    PostCountryData(newCountryData){
-
-        console.log("country: "+JSON.stringify(newCountryData))
-        axios.post('http://localhost:8080/countries',newCountryData)
+    PostCountryData(formData){
+        console.log("country: "+JSON.stringify(formData))
+        axios.post('http://localhost:8080/countries',formData)
         .then((response) => {
           console.log('Country added successfully:', response.data);
         })
         .catch((error) => {
           console.error('Error adding country:', error);
         });
-
     }
 }
-
 };
-
 </script>
